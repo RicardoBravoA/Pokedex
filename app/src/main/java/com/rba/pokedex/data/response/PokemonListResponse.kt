9be4517ed1a95db693529697979e974230cab5dd
Ricardo Bravo <ricardobravo@outlook.com>
2021-01-19 10:08:38 -0500

    feat: Add pokemon list model

--- app/src/main/java/com/rba/pokedex/data/response/PokemonListResponse.kt
+++ app/src/main/java/com/rba/pokedex/data/response/PokemonListResponse.kt
@@ -1,12 +1,15 @@
 package com.rba.pokedex.data.response
 
+import android.os.Parcelable
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 @JsonClass(generateAdapter = true)
 data class PokemonListResponse(
     @field:Json(name = "count") val count: Int,
     @field:Json(name = "next") val next: String?,
     @field:Json(name = "previous") val previous: String?,
     @field:Json(name = "results") val results: List<PokemonResponse>?
-)
+) : Parcelable

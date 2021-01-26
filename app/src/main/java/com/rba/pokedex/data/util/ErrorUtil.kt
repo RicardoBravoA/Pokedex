902064ab7efb42fada702622a45ec7a3fb5a0ff9
Ricardo Bravo <ricardobravo@outlook.com>
2021-01-26 18:22:01 -0500

    feat: Add error util

---
+++ app/src/main/java/com/rba/pokedex/data/util/ErrorUtil.kt
@@ -0,0 +1,22 @@
+package com.rba.pokedex.data.util
+
+import com.rba.pokedex.domain.model.PokemonErrorModel
+
+object ErrorUtil {
+
+    fun errorHandler(error: Throwable): PokemonErrorModel {
+
+        val errorException: RetrofitException =
+            if (error is RetrofitException) {
+                error
+            } else {
+                RetrofitException.retrofitException(error)
+            }
+
+        return when (errorException.kind) {
+            RetrofitException.Kind.HTTP -> errorException.getErrorBodyAs(PokemonErrorModel::class.java)!!
+            RetrofitException.Kind.NETWORK -> PokemonErrorModel(0, "An error occurred")
+            else -> PokemonErrorModel(0, "An error occurred")
+        }
+    }
+}

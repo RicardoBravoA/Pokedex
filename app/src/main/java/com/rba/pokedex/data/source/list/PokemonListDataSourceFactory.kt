0924960acbf129b6b50f61276114f61cb0010558
Ricardo Bravo <ricardobravo@outlook.com>
2021-01-27 17:22:07 -0500

    feat: Add pokemon list data source factory

--- app/src/main/java/com/rba/pokedex/data/source/list/PokemonListDataSourceFactory.kt
+++ app/src/main/java/com/rba/pokedex/data/source/list/PokemonListDataSourceFactory.kt
@@ -1,2 +1,23 @@
 package com.rba.pokedex.data.source.list
 
+import android.content.Context
+import com.rba.pokedex.data.storage.database.PokedexDatabase
+import com.rba.pokedex.data.util.NetworkPreference.CLOUD
+import com.rba.pokedex.data.util.NetworkPreference.DB
+import com.rba.pokedex.data.util.isInternet
+import com.rba.pokedex.domain.repository.PokemonListRepository
+
+class PokemonListDataSourceFactory(private val context: Context) {
+
+    fun create(): PokemonListRepository {
+        val database = PokedexDatabase.getDatabase(context)
+        val value = if (context.isInternet()) CLOUD else DB
+
+        return if (CLOUD == value) {
+            PokemonListRemoteDataSource(database.pokedexDao())
+        } else {
+            PokemonListLocalDataSource(database.pokedexDao())
+        }
+    }
+
+}

1db4b770670dc2ae0013bcff60f3c23da778e377
Ricardo Bravo <ricardobravo@outlook.com>
2021-01-27 16:48:55 -0500

    refactor(PokemonListRepository): Update list

--- app/src/main/java/com/rba/pokedex/domain/repository/PokemonListRepository.kt
+++ app/src/main/java/com/rba/pokedex/domain/repository/PokemonListRepository.kt
@@ -6,7 +6,7 @@ import com.rba.pokedex.domain.util.ResultType
 
 interface PokemonListRepository {
 
-    suspend fun list(): ResultType<List<PokemonListModel>, PokemonErrorModel>
+    suspend fun list(page: Int): ResultType<List<PokemonListModel>, PokemonErrorModel>
 
     suspend fun save(list: List<PokemonListModel>)
 }

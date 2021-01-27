ed80e5cded0c9b385347d14a545c089da32f1c53
Ricardo Bravo <ricardobravo@outlook.com>
2021-01-27 17:32:59 -0500

    feat(PokemonDetailRemoteDataSource): Add detail

--- app/src/main/java/com/rba/pokedex/data/source/detail/PokemonDetailRemoteDataSource.kt
+++ app/src/main/java/com/rba/pokedex/data/source/detail/PokemonDetailRemoteDataSource.kt
@@ -2,35 +2,32 @@ package com.rba.pokedex.data.source.detail
 
 import com.rba.pokedex.data.mapper.PokemonDetailMapper
 import com.rba.pokedex.data.mapper.PokemonErrorMapper
-import com.rba.pokedex.data.mapper.PokemonListMapper
 import com.rba.pokedex.data.network.ApiManager
 import com.rba.pokedex.data.storage.database.PokedexDao
 import com.rba.pokedex.data.util.ErrorUtil
 import com.rba.pokedex.data.util.RetrofitErrorUtil
 import com.rba.pokedex.domain.model.PokemonDetailModel
 import com.rba.pokedex.domain.model.PokemonErrorModel
-import com.rba.pokedex.domain.model.PokemonListModel
 import com.rba.pokedex.domain.repository.PokemonDetailRepository
-import com.rba.pokedex.domain.repository.PokemonListRepository
 import com.rba.pokedex.domain.util.ResultType
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class PokemonDetailRemoteDataSource(private val pokedexDao: PokedexDao) : PokemonDetailRepository {
 
-    override suspend fun list(page: Int): ResultType<PokemonListModel, PokemonErrorModel> {
+    override suspend fun detail(name: String): ResultType<PokemonDetailModel, PokemonErrorModel> {
         return withContext(Dispatchers.IO) {
             try {
-                val response = ApiManager.get().pokemonList(page)
+                val response = ApiManager.get().pokemonDetail(name)
                 if (response.isSuccessful) {
                     val response = response.body()!!
                     save(
-                        PokemonListMapper.transformResponseToModel(
+                        PokemonDetailMapper.transformResponseToModel(
                             response
                         )
                     )
                     ResultType.Success(
-                        PokemonListMapper.transformResponseToModel(
+                        PokemonDetailMapper.transformResponseToModel(
                             response
                         )
                     )
@@ -43,10 +40,6 @@ class PokemonDetailRemoteDataSource(private val pokedexDao: PokedexDao) : Pokemo
                 ResultType.Error(ErrorUtil.errorHandler(t))
             }
         }
-    }
-
-    override suspend fun detail(name: String): ResultType<PokemonDetailModel, PokemonErrorModel> {
-        TODO("Not yet implemented")
     }
 
     override suspend fun save(pokemonDetailModel: PokemonDetailModel) {

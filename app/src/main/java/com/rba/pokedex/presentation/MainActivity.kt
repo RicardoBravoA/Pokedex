933f57c6229f3b41250074842e34cc590ee8ea25
Ricardo Bravo <ricardobravo@outlook.com>
2021-01-28 18:04:58 -0500

    refactor: Update main location

--- app/src/main/java/com/rba/pokedex/presentation/MainActivity.kt
+++ app/src/main/java/com/rba/pokedex/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rba.pokedex
+package com.rba.pokedex.presentation
 
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -6,6 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.rba.pokedex.R
 
 class MainActivity : AppCompatActivity() {
 
